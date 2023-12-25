ef3c22d867b395d0a3ecf61f0343da9fc91c4985
Влад Нестеров <Vlanesvit@gmail.com>
2023-12-25 05:00:43 +0300

    load
    init + desk header, main, catalog

---
+++ js/rs-catalog.js
@@ -0,0 +1,154 @@
+/* ====================================
+Инициализация слайдера rs-catalog
+==================================== */
+function initCatalogSlider() {
+	if (document.querySelector('.rs-catalog__slider')) {
+		'use strict';
+		// До этой ширины слайдер будет неактивным
+		const breakpoint = window.matchMedia('(min-width: 991.98px)');
+
+		let catalogNav;
+
+		const breakpointChecker = function () {
+			if (breakpoint.matches === true) {
+				if (catalogNav !== undefined) catalogNav.destroy(true, true);
+				return;
+			} else if (breakpoint.matches === false) {
+				return enableSwiper();
+			}
+		};
+
+		// Инициализация слайдера
+		const enableSwiper = function () {
+			catalogNav = new Swiper('.rs-catalog__slider', {
+				// Автопрокрутка
+				autoplay: {
+					// Пауза между прокруткой
+					delay: 5000,
+					// Закончить на последнем слайде
+					stopOnLastSlide: false,
+					// Отключить после ручного переключения
+					disableOnInteraction: true,
+				},
+
+				// Обновить свайпер
+				// при изменении элементов слайдера
+				observer: true,
+				// при изменении родительских элементов слайдера
+				observeParents: true,
+				// при изменении дочерних элементов слайдера
+				observeSlideChildren: true,
+
+				// Скорость смены слайдов
+				speed: 500,
+
+				// Включение/отключение
+				// перетаскивание на ПК
+				simulateTouch: true,
+				// Чувствительность свайпа
+				touchRadio: 1,
+				// Угол срабатывания свайпа/перетаскивания
+				touchAngle: 45,
+				// Cобытие touchstart (pointerdown)
+				touchStartPreventDefault: true,
+
+				// Курсор перетаскивания
+				grabCursor: true,
+
+				nested: true,
+
+				// Брекпоинты (адаптив)
+				breakpoints: {
+					320: {
+						slidesPerView: 1.5,
+						spaceBetween: 40,
+					},
+					540: {
+						slidesPerView: 2,
+						spaceBetween: 40,
+					},
+				},
+			});
+		};
+
+		breakpoint.addListener(breakpointChecker);
+		breakpointChecker();
+	}
+}
+
+/* ====================================
+Инициализация слайдера в блоке product
+==================================== */
+function initBlockSliders() {
+	const sliderBlocks = document.querySelectorAll('.product__img');
+
+	sliderBlocks.forEach(block => {
+		const slider = block.querySelector('.product__img_slider')
+		const sliderPag = block.querySelector('.product__img_pagination')
+
+		// Перечень слайдеров
+		if (slider) {
+			const swiperMain = new Swiper(slider, {
+				// Обновить свайпер
+				// при изменении элементов слайдера
+				observer: true,
+				// при изменении родительских элементов слайдера
+				observeParents: true,
+				// при изменении дочерних элементов слайдера
+				observeSlideChildren: true,
+
+				// Скорость смены слайдов
+				speed: 500,
+
+				// Включение/отключение
+				// перетаскивание на ПК
+				simulateTouch: true,
+				// Чувствительность свайпа
+				touchRadio: 1,
+				// Угол срабатывания свайпа/перетаскивания
+				touchAngle: 45,
+				// Cобытие touchstart (pointerdown)
+				touchStartPreventDefault: true,
+
+				// Курсор перетаскивания
+				grabCursor: true,
+
+				// Пагинация
+				pagination: {
+					el: sliderPag,
+					clickable: true,
+					// type: 'progressbar',
+				},
+
+				effect: "fade",
+				slidesPerView: 1,
+				spaceBetween: 0,
+			});
+		}
+	});
+
+}
+
+window.addEventListener("load", function (e) {
+	// Запуск инициализации слайдера
+	initCatalogSlider();
+	initBlockSliders();
+});
+
+/* ====================================
+Анимация кнопки корзины
+==================================== */
+const productItem = document.querySelectorAll('.product');
+
+productItem.forEach(product => {
+	const cartBtn = product.querySelectorAll('.cart-btn');
+
+	cartBtn.forEach(btn => {
+		btn.addEventListener('click', function (e) {
+			e.preventDefault();
+			cartBtn.forEach(btnAll => {
+				btnAll.classList.toggle('_add-to-cart');
+			});
+		})
+	});
+});
---
+++ js/rs-header.js
@@ -0,0 +1,388 @@
+/* ====================================
+Меню
+==================================== */
+function menuFunction() {
+	const menus = document.querySelectorAll('.rs-header__menu');
+
+	// Бургер-кнопка
+	function menuBurger() {
+		menus.forEach(menu => {
+			const menuBurgerBtns = menu.querySelectorAll('.menu__icon');
+
+			if (menuBurgerBtns) {
+				menuBurgerBtns.forEach(btn => {
+					// Открываем меню
+					btn.addEventListener("click", function (e) {
+						if (document.documentElement.classList.contains("menu-open")) {
+							menuClose();
+						} else {
+							menuOpen()
+						}
+					});
+				});
+			}
+		});
+	};
+	if (document.querySelector(".menu__icon")) {
+		menuBurger()
+	}
+
+	// Меню
+	function menuInit() {
+		menus.forEach(menu => {
+			// Все пункты
+			const menuItem = menu.querySelectorAll('.menu__list li');
+
+			// Все пункты с выпадающим меню
+			const menuItemDropdowns = menu.querySelectorAll('.menu__list .dropdown');
+			const menuItemDropdownsMenu = menu.querySelectorAll('.menu__list .dropdown__menu');
+
+			// 0-ой уровень
+			const menuItemDropdownsNull = menu.querySelectorAll('.menu__list > .dropdown');
+			const menuItemDropdownsMenuNull = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu');
+
+			// 1-ый уровень
+			const menuItemDropdownsFirst = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown');
+			const menuItemDropdownsMenuFirst = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu');
+
+			// 2-ой уровень
+			const menuItemDropdownsTwo = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu > .dropdown');
+			const menuItemDropdownsMenuTwo = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu');
+
+			// 3-ий уровень
+			const menuItemDropdownsThree = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu > .dropdown  > .dropdown__menu > .dropdown');
+			const menuItemDropdownsMenuThree = menu.querySelectorAll('.menu__list > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu > .dropdown > .dropdown__menu');
+
+			// Добавляем иконки в пункты с выпадающим меню
+			menuItemDropdowns.forEach(item => {
+				const menuLink = item.querySelector('a');
+				let icon = document.createElement('i');
+				menuLink.append(icon);
+			});
+
+			// Функция для отдельных уровней меню, чтобы открывался только один пункт, а открытые закрывались, кроме тех, кто выше уровнем
+			function openLvlMenu(li, ul) {
+				li.forEach(item => {
+					const menuItemList = item.querySelector('ul');
+					const menuItemIcons = item.querySelector('a > i');
+
+					// Раскрываем меню при клике на иконку
+					menuItemIcons.addEventListener('click', (e) => {
+						e.preventDefault();
+						_slideToggle(menuItemList, 500);
+						ul.forEach(menu => {
+							if (!menu.hasAttribute('hidden')) {
+								_slideUp(menu, 500);
+							}
+						});
+
+						// Проходимся по всем пунктам и ищем активные классы, убираем их и добавляем активный класс кликнутому пункту
+						if (!menuItemIcons.closest('.dropdown').classList.contains('_open-menu')) {
+							li.forEach(itemDrop => {
+								if (itemDrop.classList.contains('_open-menu')) {
+									itemDrop.classList.remove('_open-menu')
+								}
+							});
+							menuItemIcons.closest('.dropdown').classList.add('_open-menu');
+						} else if (menuItemIcons.closest('.dropdown').classList.contains('_open-menu')) {
+							menuItemIcons.closest('.dropdown').classList.remove('_open-menu');
+						}
+					});
+				});
+			}
+
+			// Пункты 0-го уровня меню
+			openLvlMenu(menuItemDropdownsNull, menuItemDropdownsMenuNull)
+			// Пункты 1-го уровня меню
+			openLvlMenu(menuItemDropdownsFirst, menuItemDropdownsMenuFirst)
+			// Пункты 2-го уровня меню
+			openLvlMenu(menuItemDropdownsThree, menuItemDropdownsMenuTwo)
+			// Пункты 3-го уровня меню
+			openLvlMenu(menuItemDropdownsTwo, menuItemDropdownsMenuThree)
+
+			// При клике на бургер убираем открые меню и активные класс
+			document.addEventListener("click", function (e) {
+				if (e.target.closest('.menu__icon')) {
+					menuItemDropdownsMenu.forEach(menu => {
+						_slideUp(menu, 500);
+					});
+					menuItemDropdowns.forEach(item => {
+						item.classList.remove('_open-menu');
+					});
+				}
+			});
+		});
+	}
+	menuInit()
+
+	// Функции открытия бургер-меню с блокировкой скролла
+	function menuOpen() {
+		bodyLock();
+		document.documentElement.classList.add("menu-open");
+	}
+	function menuClose() {
+		bodyUnlock();
+		document.documentElement.classList.remove("menu-open");
+	}
+	function menuToggle() {
+		bodyLockToggle();
+		document.documentElement.classList.toggle("menu-open");
+	}
+}
+menuFunction()
+
+/* ====================================
+Якорные ссылки
+==================================== */
+// data-goto - указать ID блока
+// data-goto-header - учитывать header
+// data-goto-top - недокрутить на указанный размер
+// data-goto-speed - скорость (только если используется доп плагин)
+let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+	const targetBlockElement = document.querySelector(targetBlock);
+	if (targetBlockElement) {
+		let headerItem = "";
+		let headerItemHeight = 0;
+		if (noHeader) {
+			headerItem = ".header";
+			const headerElement = document.querySelector(headerItem);
+			if (!headerElement.classList.contains("_header-scroll")) {
+				headerElement.style.cssText = `transition-duration: 0s;`;
+				headerElement.classList.add("_header-scroll");
+				headerItemHeight = headerElement.offsetHeight;
+				headerElement.classList.remove("_header-scroll");
+				setTimeout((() => {
+					headerElement.style.cssText = ``;
+				}), 0);
+			} else headerItemHeight = headerElement.offsetHeight;
+		}
+		let options = {
+			speedAsDuration: true,
+			speed,
+			header: headerItem,
+			offset: offsetTop,
+			easing: "linear"
+		};
+		document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+		if ("undefined" !== typeof SmoothScroll) (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
+			let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+			targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+			targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+			window.scrollTo({
+				top: targetBlockElementPosition,
+				behavior: "smooth"
+			});
+		};
+	};
+}
+function pageNavigation() {
+	document.addEventListener("click", pageNavigationAction);
+	document.addEventListener("watcherCallback", pageNavigationAction);
+	function pageNavigationAction(e) {
+		if ("click" === e.type) {
+			const targetElement = e.target;
+			if (targetElement.closest("[data-goto]")) {
+				const gotoLink = targetElement.closest("[data-goto]");
+				const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
+				const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
+				const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
+				const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
+				gotoblock_gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
+				e.preventDefault();
+			}
+		} else if ("watcherCallback" === e.type && e.detail) {
+			const entry = e.detail.entry;
+			const targetElement = entry.target;
+			if ("navigator" === targetElement.dataset.watch) {
+				document.querySelector(`[data-goto]._navigator-active`);
+				let navigatorCurrentItem;
+				if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`); else if (targetElement.classList.length) for (let index = 0; index < targetElement.classList.length; index++) {
+					const element = targetElement.classList[index];
+					if (document.querySelector(`[data-goto=".${element}"]`)) {
+						navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
+						break;
+					}
+				}
+				if (entry.isIntersecting) navigatorCurrentItem ? navigatorCurrentItem.classList.add("_navigator-active") : null; else navigatorCurrentItem ? navigatorCurrentItem.classList.remove("_navigator-active") : null;
+			}
+		}
+	}
+}
+pageNavigation();
+
+/* ====================================
+Header при скролле
+==================================== */
+function headerFixed() {
+	const header = document.querySelector('.rs-header');
+	const headerFixed = document.querySelector('.rs-header__fixed');
+	let lastScrollTop = 0;
+
+	function headerClassAdd() {
+		let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+		if (window.scrollY >= 500) {
+			// Проверка на присутствие класса для бургер-меню. Если он есть, то шапка не скрывается
+			if (document.documentElement.classList.contains("menu-open")) {
+				headerFixed.style.top = "0px";
+			}
+			else {
+				// Скрытие шапки
+				if (scrollTop > lastScrollTop) {
+					headerFixed.style.top = "-300px";
+				} else {
+					headerFixed.style.top = "0px";
+				}
+			}
+		} else {
+			headerFixed.style.top = "-300px";
+		}
+		lastScrollTop = scrollTop;
+	}
+
+	window.addEventListener('scroll', function () {
+		headerClassAdd()
+	})
+	window.addEventListener('load', function () {
+		headerClassAdd()
+	})
+	window.addEventListener('resize', function () {
+		headerClassAdd()
+	})
+}
+headerFixed()
+
+/* ====================================
+Поиск
+==================================== */
+function search() {
+	const searchs = document.querySelectorAll('.search');
+
+	searchs.forEach(search => {
+		const searchSubmit = search.querySelector('.search__submit')
+		const searchClear = search.querySelector('.search__clear');
+		const searchInput = search.querySelector('.search__input')
+		const searchForm = search.querySelector('.search__form');
+		const searchModal = document.querySelector('.search__block');
+		const searchShows = document.querySelector('.search__show');
+
+		searchShows.addEventListener('click', function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			searchToggle()
+			putСursorInInput(searchInput);
+		})
+
+		// Закрываем поиск по оверлею
+		searchModal.addEventListener('click', function (e) {
+			const target = e.target;
+			// Делегируем событие
+			if (target.classList.contains('search__close')) {
+				searchClose()
+			}
+		});
+		searchModal.addEventListener('click', function (e) {
+			e.stopPropagation();
+		});
+		document.addEventListener('click', function (e) {
+			searchClose()
+		});
+
+		// Отправка формы
+		searchSubmit.addEventListener('click', function (e) {
+			e.preventDefault();
+			if (searchInput.value != '') {
+				searchForm.submit();
+			}
+		})
+
+		// При вводе появляется кнопка отчистки
+		searchInput.addEventListener('input', function (e) {
+			searchClear.style.display = "flex";
+		})
+
+		// Очистить инпут
+		searchClear.addEventListener('click', function (e) {
+			searchInput.value = '';
+			searchClear.style.display = "none";
+			putСursorInInput(searchInput);
+		})
+	});
+
+	// Вспомогательные функции ========================================================================================================================================================
+	// Поставить курсор в инпут после клика
+	function putСursorInInput(input) {
+		setTimeout(function () {
+			input.focus()
+		}, 0);
+	}
+	// Функции открытия/закрытия поиска с блокировкой скролла
+	function searchOpen() {
+		// bodyLock();
+		document.documentElement.classList.add("search-open");
+	}
+	function searchClose() {
+		// bodyUnlock();
+		document.documentElement.classList.remove("search-open");
+	}
+	function searchToggle() {
+		// bodyLockToggle();
+		document.documentElement.classList.toggle("search-open");
+	}
+}
+if (document.querySelector('.search')) {
+	search()
+}
+
+/* ====================================
+Слайдер категорий
+==================================== */
+function initHeadCate() {
+	const sliderBlocks = document.querySelectorAll('.rs-header__category');
+
+	sliderBlocks.forEach(block => {
+		const slider = block.querySelector('.rs-header__category_slider')
+		const sliderNext = block.querySelector('.rs-header__category_button-next')
+		const sliderPrev = block.querySelector('.rs-header__category_button-prev')
+
+		// Перечень слайдеров
+		if (slider) {
+			const swiperMain = new Swiper(slider, {
+				// Обновить свайпер
+				// при изменении элементов слайдера
+				observer: true,
+				// при изменении родительских элементов слайдера
+				observeParents: true,
+				// при изменении дочерних элементов слайдера
+				observeSlideChildren: true,
+
+				// Скорость смены слайдов
+				speed: 500,
+
+				// Включение/отключение
+				// перетаскивание на ПК
+				simulateTouch: true,
+				// Чувствительность свайпа
+				touchRadio: 1,
+				// Угол срабатывания свайпа/перетаскивания
+				touchAngle: 45,
+				// Cобытие touchstart (pointerdown)
+				touchStartPreventDefault: true,
+
+				// Курсор перетаскивания
+				grabCursor: true,
+
+				// Стрелки
+				navigation: {
+					nextEl: sliderNext,
+					prevEl: sliderPrev,
+				},
+
+				slidesPerView: 'auto',
+				spaceBetween: 20,
+			});
+		}
+	});
+}
+// Запуск инициализации слайдеров
+initHeadCate();
